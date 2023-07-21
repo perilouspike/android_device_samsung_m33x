3f6658c88b6dc9f5c3dc543126a37cc4745acb09
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2023-07-22 02:43:59 +0530

    Tree Rebased.

--- twrp_m33.mk
+++ twrp_m33.mk
@@ -19,8 +19,11 @@ DEVICE_PATH := device/samsung/m33
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
@@ -28,15 +31,21 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from m33 device
 $(call inherit-product, device/samsung/m33/device.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a33x/recovery/root,recovery/root)
+
 PRODUCT_DEVICE := m33
 PRODUCT_NAME := twrp_m33
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-M336BU
 PRODUCT_MANUFACTURER := samsung
 
-PRODUCT_GMS_CLIENTID_BASE := android-samsung
+#PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
+#PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="m33xins-user 12 SP1A.210812.016 M336BUXXS4BWB1 release-keys"
 
-BUILD_FINGERPRINT := samsung/m33xins/m33x:12/SP1A.210812.016/M336BUXXS4BWB1:user/release-keys
+#BUILD_FINGERPRINT := samsung/m33xins/m33x:12/SP1A.210812.016/M336BUXXS4BWB1:user/release-keys
